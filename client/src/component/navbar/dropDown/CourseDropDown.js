38f2f87269fecd82988cbdff4bcb380d5603fc4e
image4u <nadeem4444@gmail.com>
2023-02-11 17:37:37 +0530

    fix:ancor

--- client/src/component/navbar/dropDown/CourseDropDown.js
+++ client/src/component/navbar/dropDown/CourseDropDown.js
@@ -102,7 +102,7 @@ export default function MenuListComposition() {
                                         <div style={{height: "100%",overflow: "overlay"}}>
                                         {SyllabusItem.map((item,index) => {
                                             return (
-                                              <a style={{ position: "relative", overflow: "visible" }} href={require(`../../../paramedicalSyllabus/${index}.pdf`)}>{item}</a>
+                                              <a className='hovercolor' style={{ position: "relative", overflow: "visible" }} href={require(`../../../paramedicalSyllabus/${index}.pdf`)}>{item}</a>
 
                                             )
 
